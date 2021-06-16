acc0bbb03ab84356d6dfaa1f7ae64db9d63e69a4
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2021-06-16 15:36:10 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,39 @@
+#print letter O
+print('######\t \n    \n#    #\n   \n#    #\n   \n#    #\n###### ')
+#print letter H
+
+print('#    #\n     \n#    #\n    \n######\n    \n#    #\n    \n#    #\n')
+
+#alternative :)
+a = "###########"
+b = "           "
+c = "#         #"
+d = "           "
+e = "#         #"
+f = "           "
+g = "#         #"
+h = "###########"
+#letter O
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+#letter H
+print(c)
+print(d)
+print(c)
+print(d)
+print(a)
+print(b)
+print(c)
+print(d)
+print(c)
+print(d)
+
+
+
+
