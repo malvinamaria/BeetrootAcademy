9ec21aedc60de6c95f26748b6bea8ea2748ad2aa
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2021-06-28 13:40:59 +0200

    Update wednesdsay_w25.py

--- week_25/wednesdsay_w25.py
+++ week_25/wednesdsay_w25.py
@@ -4,4 +4,4 @@ random_number_generator = (random.uniform(1, 10))
 if random_number_generator == guessing_input:
     print("Correct guess")
 elif guessing_input != random_number_generator:
-    print(f"Inorecct guess, the correct number was:", {random_number_generator})
+    print(f"Inorecct guess, the correct number was:", random_number_generator)
