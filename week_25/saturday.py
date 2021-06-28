e1eee35b39caafc41b67009d2df19d34b1f5e939
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2021-06-28 15:59:32 +0200

    Update saturday.py

--- week_25/saturday.py
+++ week_25/saturday.py
@@ -11,3 +11,38 @@ for i in range(0, 10):
     
 print(randomlist)
 print(max(randomlist))
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+#task 2
+
+import random
+
+randomlist_1 = []
+
+for i in range(0, 10):
+    random_generator = random.randint(1, 10)
+    randomlist_1.append(random_generator)
+    
+    
+print(randomlist_1)
+
+randomlist_2 = []
+
+for i in range(0, 10):
+    random_generator = random.randint(1, 10)
+    randomlist_2.append(random_generator)
+  
+    
+print(randomlist_2)
+
+
+# remove duplicates from list
+randomlist_1.extend(randomlist_2)
+randomlist_1 = list(dict.fromkeys(randomlist_1)) 
+print(randomlist_1)
+
+#dict.fromkeys(randomlist_1)
+# Create a dictionary, using the List items as keys. This will automatically remove any duplicates because dictionaries cannot have duplicate keys.
+
+#randomlist_1 = list
+#Then, convert the dictionary back into a list:
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
