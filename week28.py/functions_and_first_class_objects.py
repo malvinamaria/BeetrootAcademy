38e38118a37325da9f12590e30f6bb1c17077b70
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2021-07-16 15:17:41 +0200

    Create functions_and_first_class_objects.py

---
+++ week28.py/functions_and_first_class_objects.py
@@ -0,0 +1,14 @@
+# TASK ONE
+
+def function():
+    x = 1
+    y = 2
+    z = 5
+    str1 = "something"
+print("Detect number of local variables declared in a function:")
+
+print(function.__code__.co_nlocals)
+print(function.__code__)  # code object function at 0x10b1d5660
+
+
+# TASK TWO
